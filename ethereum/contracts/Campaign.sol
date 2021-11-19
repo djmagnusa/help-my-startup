d969a2bc55505dfceaaf0dcbb0f5cb0818d8a1d9
Pratush <pratush.bh@gmail.com>
2021-11-19 18:04:47 +0530

    Set up required variables

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.17;
 
 contract Campaign {
     
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+
     function Campaign(uint minimum) public{
         manager = msg.sender;
         minimumContribution = minimum;
