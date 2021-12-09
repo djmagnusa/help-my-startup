abbfa1ea4454ccfffca1a0082054f8d3b30232e9
Pratush <pratush.bh@gmail.com>
2021-12-09 22:07:29 +0530

    Add request struct

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount; //for only people voting yes
+        mapping(address => bool) approvals; //people who provided the approval of this request
+    }
     
     Request[] public requests;
     address public manager;
