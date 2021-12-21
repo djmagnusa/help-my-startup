fb6775ea81d26bad2c1cad1f4202ad5134cdbd69
Pratush <pratush.bh@gmail.com>
2021-12-21 16:15:19 +0530

    Render campaign card and create campaign button

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        // console.log("Hello");
-        console.log(campaigns);
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return { campaigns: campaigns }
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaigns</a>,
+                fluid: true  
+            };
+        });
+
+        return <Card.Group items={items} />;
     }
 
     render () {
-        return <div>Campaigns Index!</div>
+        return <div>
+            <link
+                async
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+            />
+            <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+            {this.renderCampaigns()}
+            <Button 
+                content="Create campaign"
+                icon="add circle"
+                primary
+            />
+            </div>
     }
 }
 
