4c304709ace21f3fc61d195215d7e1604b2746f7
Pratush <pratush.bh@gmail.com>
2021-12-22 13:24:01 +0530

    Add button spinner

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -8,12 +8,15 @@ class CampaignNew extends Component {
 
     state = {
         minimumContribution: '',
-        errorMessage: ''
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async event => {
         event.preventDefault();
 
+        this.setState({ loading: true, errorMessage: '' });
+        
         try{
             const accounts = await web3.eth.getAccounts();
             await factory.methods.createCampaign(this.state.minimumContribution).send({
@@ -22,6 +25,8 @@ class CampaignNew extends Component {
         } catch(err) {
             this.setState({ errorMessage: err.message })
         }
+
+        this.setState({ loading: false });
     }
 
     render() {
@@ -41,7 +46,7 @@ class CampaignNew extends Component {
                     </Form.Field>
 
                     <Message error header="Oops!" content={this.state.errorMessage} />
-                    <Button primary>Create!</Button>
+                    <Button loading={this.state.loading} primary>Create!</Button>
                 </Form>
             </Layout>
         );  
