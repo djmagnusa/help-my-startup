0ea8ed6a5f8a7be391a745600b501d83fdd3c158
Pratush <pratush.bh@gmail.com>
2021-12-22 15:06:05 +0530

    Automatic and header navigation

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
     return (
         <Menu style={{ marginTop:'10px' }}>
 
-            <Menu.Item>
-                HelpMyStartup
-            </Menu.Item>
+            <Link route="/">
+
+                <a className="item">
+                    HelpMyStartup
+                </a>
+            
+            </Link>
 
             <Menu.Menu position="right">
-                <Menu.Item>
-                    Campaigns
-                </Menu.Item>
+                
+                <Link route="/">
+                    <a className="item">Campaigns</a>
+                </Link>
+                
+                <Link route="/campaigns/new">
+                    <a className="item">+</a>
+                </Link>
 
-                <Menu.Item>
-                    +
-                </Menu.Item>
             </Menu.Menu>
         </Menu>
     );
