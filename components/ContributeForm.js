ee23871813879b594e140fb49e954903e31e3b7a
Pratush <pratush.bh@gmail.com>
2021-12-23 20:51:52 +0530

    Add error message and spinner for contribution

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -7,13 +7,17 @@ import { Router } from '../routes';
 class ContributeForm extends Component {
 
     state = {
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
 
         const campaign = Campaign(this.props.address);
+
+        this.setState({ loading: true });
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -24,14 +28,16 @@ class ContributeForm extends Component {
 
             Router.replaceRoute(`/campaigns/${this.props.address}`) //refresh the page
         } catch(err) {
-
+            this.setState({ errorMessage: err.message })
         }
+
+        this.setState({ loading: false, errorMessage: '' });
     };
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
-                <Form.Field>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage }>
+                <Form.Field> 
                     <label>Amount to Contribute</label>
                     <Input
                         value={this.state.value}
@@ -40,7 +46,9 @@ class ContributeForm extends Component {
                         labelPosition="right"
                     />
                 </Form.Field>
-                <Button primary>
+
+                <Message error header="Oops!" content={this.state.errorMessage} />
+                <Button primary loading={this.state.loading}>
                     Contribute!
                 </Button>
             </Form>
